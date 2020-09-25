195ab47103c7680b3fff29b393f60f42a7b126a3
Will Holland <snowy.the.crow@gmail.com>
2020-09-25 19:15:48 +0700

    modify to work with esp32

--- ArduinoLight.ino
+++ ArduinoLight.ino
@@ -2,20 +2,16 @@
 //have to use the default Serial
 //so the Arduino can be plugged in
 //to a computer and still use bluetooth
-#include <SoftwareSerial.h>
+#include "BluetoothSerial.h"
 
-//Define the pins used for receiving
-//and transmitting information via Bluetooth
-const int rxpin = 2;
-const int txpin = 3;
 //Variable to store input value
 //initialized with arbitrary value
 char k = 'A';
 //Connect the Bluetooth module
-SoftwareSerial bluetooth(rxpin, txpin);
+BluetoothSerial bluetooth;
 
 //Define the pin to control the light
-int lightbulb = 7
+int lightbulb = 2
 
 void setup()
 {
@@ -25,7 +21,7 @@ void setup()
   Serial.begin(9600);
   Serial.println("Serial ready");
   //Initialize the bluetooth
-  bluetooth.begin(9600);
+  bluetooth.begin("ESP32");
   bluetooth.println("Bluetooth ready");
 }
 
@@ -40,11 +36,11 @@ void loop()
   }
   //Turn on the light if transmitted data is H
   if( k == 'H' ){
-    digitalWrite(7, HIGH);
+    digitalWrite(2, HIGH);
   }
   //Turn off the light if transmitted data is L
   else if( k == 'L') {
-    digitalWrite(7, LOW);
+    digitalWrite(2, LOW);
   }
   //Wait ten milliseconds to decrease unnecessary hardware strain
    delay(10);
